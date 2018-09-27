768433476a3e70318dd60279e705bacd155c51a0
wangbin <asample@wanda.cn>
2018-09-27 13:56:44 +0800

    Initial commit

---
+++ src/main/java/discard/DiscardServerHandler.java
@@ -0,0 +1,33 @@
+package discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 处理服务端通道
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // 发生异常时关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
